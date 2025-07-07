23d66de9f63ff4d185550272b2ee80feacf329ff
venkatavvari3 <133982540+venkatavvari3@users.noreply.github.com>
2025-07-07 16:54:35 +0000

    modify refresh interval for ppd

--- src/App.js
+++ src/App.js
@@ -829,7 +829,7 @@ function PPDashboard({ token, onLogout, persona, loginName }) {
 
   useEffect(() => {
     fetchData();
-    const interval = setInterval(fetchData, 60000);
+    const interval = setInterval(fetchData, 180000);
     return () => clearInterval(interval);
   }, [token]);
 
