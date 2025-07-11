b419ddd227f2899afe0c6d9e91135d7be43e4a39
venkatavvari3 <133982540+venkatavvari3@users.noreply.github.com>
2025-07-11 18:33:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -912,17 +912,17 @@ if (loading) return <Spinner animation="border" />;
 
 {selectedVisuals.chart && (
   <Row>
-       <Col md={3} className="mb-4">
-          <Card>
-            <Card.Body style={{ minHeight: 220, height: 220, display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 0 }}>
-              <div ref={treemapRef} style={{ width: "99%", height: "99%" }}></div>
-            </Card.Body>
-            <Card.Footer className="text-center">Revenue Treemap</Card.Footer>
-          </Card>
-        </Col>
-      </Row>
+    <Col md={3} className="mb-4">
+      <Card>
+        <Card.Body style={{ minHeight: 220, height: 220, display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 0 }}>
+          <div ref={treemapRef} style={{ width: "99%", height: "99%" }}></div>
+        </Card.Body>
+        <Card.Footer className="text-center">Revenue Treemap</Card.Footer>
+      </Card>
+    </Col>
   </Row>
 )}
+        
       <div ref={tableRef}>
         <Table striped bordered hover size="sm" className="mt-4">
           <thead>
